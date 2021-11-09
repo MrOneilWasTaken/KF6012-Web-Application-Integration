e06e8affc836ec285a857b3a5682e7db38abcee3
Sam <samoneil76@gmail.com>
2021-11-09 06:02:46 +0000

    cleaning

--- week6/src/htmlresponse.php
+++ week6/src/htmlresponse.php
@@ -4,6 +4,5 @@
         protected function headers(){
             header("Access-Control-Allow-Origin: *");
             header("Content-Type: text/html; charset=UTF-8");
-        }
+        } 
     }
-?>
